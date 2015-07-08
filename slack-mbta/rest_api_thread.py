b3a21b028935d0141b0140addcf6b80cf7b04f9c
Daniel Fithian <daniel.m.fithian@gmail.com>
2015-07-08 19:28:20 -0400

    better separate threads

--- slack-mbta/rest_api_thread.py
+++ slack-mbta/rest_api_thread.py
@@ -2,22 +2,11 @@ import threading
 import web
 import json
 import logging
-from config import Config
-from output import OutputFactory
 from transaction import TransactionContext
 from route_resolver import resolve_route
 from slack_messaging_thread import SlackMessagingThread
 
 log = logging.getLogger(__name__)
-def config():
-    return Config.instance()
-def outputter():
-    return OutputFactory.get_outputter(config())
-
-def transaction_flow(txn_context, config, outputter):
-    txn_context.do_transaction()
-    return outputter.format(txn_context.request, txn_context.reply)
-
 class get_bus:
     def GET(self):
         input_params = dict(
@@ -25,16 +14,7 @@ class get_bus:
             user=web.input().user_name
         )
         log.info("Received request for bus: {0}".format(json.dumps(input_params)))
-        payload = transaction_flow(
-            TransactionContext.MBTA_BUS_TXN_CONTEXT(
-                config(),
-                input_params
-            ),
-            config(),
-            outputter()
-        )
-        SlackMessagingThread.queue.put(payload)
-        log.info("Enqueued payload {0}".format(payload))
+        SlackMessagingThread.queue.put((TransactionContext.MBTA_BUS_TXN_CONTEXT, input_params))
         return ''
 
 class get_alert:
@@ -44,16 +24,7 @@ class get_alert:
             user=web.input().user_name
         )
         log.info("Received request for alert: {0}".format(json.dumps(input_params)))
-        payload = transaction_flow(
-            TransactionContext.MBTA_ALERT_TXN_CONTEXT(
-                config(),
-                input_params
-            ),
-            config(),
-            outputter()
-        )
-        SlackMessagingThread.queue.put(payload)
-        log.info("Enqueued payload {0}".format(payload))
+        SlackMessagingThread.queue.put((TransactionContext.MBTA_ALERT_TXN_CONTEXT, input_params))
         return ''
 
 class get_alerts:
@@ -62,16 +33,7 @@ class get_alerts:
             user=web.input().user_name
         )
         log.info("Received request for alerts: {0}".format(json.dumps(input_params)))
-        payload = transaction_flow(
-            TransactionContext.MBTA_ALERTS_TXN_CONTEXT(
-                config(),
-                input_params
-            ),
-            config(),
-            outputter()
-        )
-        SlackMessagingThread.queue.put(payload)
-        log.info("Enqueued payload {0}".format(payload))
+        SlackMessagingThread.queue.put((TransactionContext.MBTA_ALERTS_TXN_CONTEXT, input_params))
         return ''
 
 class RestAPIThread(threading.Thread):
--- slack-mbta/slack_messaging_thread.py
+++ slack-mbta/slack_messaging_thread.py
@@ -2,11 +2,20 @@ import logging
 import time
 import threading
 from config import Config
+from output import OutputFactory
 from transaction import TransactionContext
 from Queue import Queue
 
 # read queue every 2 seconds and process everything in there
 log = logging.getLogger(__name__)
+def config():
+    return Config.instance()
+def outputter():
+    return OutputFactory.get_outputter(config())
+def transaction_flow(txn_context, config, outputter):
+    txn_context.do_transaction()
+    return outputter.format(txn_context.request, txn_context.reply)
+
 class SlackMessagingThread(threading.Thread):
     queue = Queue()
     def __init__(self, *args, **kwargs):
@@ -16,9 +25,11 @@ class SlackMessagingThread(threading.Thread):
             time.sleep(2)
             while not SlackMessagingThread.queue.empty():
                 try:
-                    payload_to_process = SlackMessagingThread.queue.get()
-                    txn_context = TransactionContext.SLACK_TXN_CONTEXT(Config.instance(), payload_to_process)
-                    txn_context.do_transaction()
+                    (constructor, input_params) = SlackMessagingThread.queue.get()
+                    txn_context_mbta = constructor(config(), input_params)
+                    payload = transaction_flow(txn_context_mbta, config(), outputter())
+                    txn_context_slack = TransactionContext.SLACK_TXN_CONTEXT(config(), payload)
+                    txn_context_slack.do_transaction()
                 except Exception:
                     log.exception('Got exception processing payload {0}'.format(payload_to_process))
 
