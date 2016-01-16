9b8dbbe5bcc5acd0929f217429b46f6ce406b0f2
Daniel Fithian <daniel.m.fithian@gmail.com>
2016-01-16 00:08:58 -0500

    fix config

--- slack-mbta/config.py
+++ slack-mbta/config.py
@@ -5,19 +5,16 @@ from output import SlackOutputter
 log = logging.getLogger(__name__)
 class Config(object):
     def __init__(self, file):
-        if file is None:
+        try:
+            log.info('loading config')
+            self.load(json.loads(open(file, 'r').read()))
+        except Exception:
+            log.exception('failed to load config due to exception')
             self.load({})
-        else:
-            try:
-                self.load(json.loads(open(file, 'r').read()))
-            except Exception:
-                log.exception('failed to load config due to exception')
-            finally:
-                self.load({})
     def load(self, json):
-        self.host = getattr(json, 'host', 'localhost')
-        self.port = getattr(json, 'port', 8080)
-        self.max_bus_lines = getattr(json, 'max_bus_lines', 3)
-        self.max_alerts = getattr(json, 'max_alerts', 3)
+        self.host = json.get('host')
+        self.port = json.get('port')
+        self.max_bus_lines = json.get('max_bus_lines', 3)
+        self.max_alerts = json.get('max_alerts', 3)
         self.outputter = SlackOutputter()
-        self.webhook_url = getattr(json, 'webhook_url', 'https://hooks.slack.com/services/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')
+        self.webhook_url = json.get('webhook_url')
